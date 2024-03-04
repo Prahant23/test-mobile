4d3a0921b4918761521a415196c3f3a685a96037
PrashantBista <prashantbist64@gmail.com>
2024-03-04 15:23:50 +0545

    dsahboard profile

--- lib/views/dashboard/dashboard_profile.dart
+++ lib/views/dashboard/dashboard_profile.dart
@@ -9,116 +9,106 @@ class MyProfile extends StatefulWidget {
 
 class _MyProfileScreenState extends State<MyProfile> {
   final ScrollController _scrollController = ScrollController();
-  List<String> clientsList = []; // Replace this with your actual data list
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_scrollListener);
-    // Load initial data or call your API to fetch data here
-    loadInitialData();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _refreshData() async {
-    // Add your logic to refresh data (e.g., API call)
-    // Clear the existing data
-    clientsList.clear();
-    // Load fresh data
-    loadInitialData();
-  }
-
-  void _scrollListener() {
-    if (_scrollController.position.pixels ==
-        _scrollController.position.maxScrollExtent) {
-      // User has reached the end of the list, load more data
-      if (!isLoading) {
-        // Set isLoading to true to prevent multiple simultaneous requests
-        setState(() {
-          isLoading = true;
-        });
-
-        // Delay for 2 seconds before loading more data
-        Future.delayed(const Duration(seconds: 2), () {
-          loadMoreData();
-          // Set isLoading back to false after loading is complete
-          setState(() {
-            isLoading = false;
-          });
-        });
-      }
-    }
-  }
-
-  void loadInitialData() {
-    // Replace this with your logic to fetch initial data (e.g., API call)
-    // Add data to the list
-    for (int i = 0; i < 20; i++) {
-      clientsList.add('Client ${i + 1}');
-    }
-    // Trigger a rebuild to update the UI
-    setState(() {});
-  }
-
-  void loadMoreData() {
-    // Replace this with your logic to fetch more data (e.g., API call)
-    // Append the new data to the existing list
-    for (int i = 0; i < 10; i++) {
-      clientsList.add('New Client ${clientsList.length + 1}');
-    }
-    // Trigger a rebuild to update the UI
-    setState(() {});
-  }
+  bool _isEditing = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat'),
-      ),
-      body: RefreshIndicator(
-        onRefresh: _refreshData,
-        child: SingleChildScrollView(
-          controller: _scrollController,
-          child: Column(
-            children: [
-              // Your existing MyProfileScreen content here
-
-              // Clients list
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: clientsList.length + 1, // +1 for loading indicator
-                itemBuilder: (context, index) {
-                  if (index < clientsList.length) {
-                    // Display your client item (similar to Instagram messages)
-                    return ListTile(
-                      title: Text(clientsList[index]),
-                      subtitle: Text('Last message from ${clientsList[index]}'),
-                      leading: CircleAvatar(
-                        // Add your client's avatar here
-                        child: Text('${clientsList[index][0]}'),
-                      ),
-                      // Add other client information/widgets here
-                    );
-                  } else {
-                    // Loading indicator while fetching more data
-                    return isLoading
-                        ? const Center(
-                            child: CircularProgressIndicator(),
-                          )
-                        : Container(); // Show an empty container when not loading
-                  }
-                },
-              ),
-            ],
+        title: Text('Profile'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _isEditing = !_isEditing;
+              });
+            },
+            icon: Icon(_isEditing ? Icons.done : Icons.edit),
           ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Your New Profile',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue, // Adjust color as needed
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              height: MediaQuery.of(context).size.height -
+                  kToolbarHeight -
+                  200, // Adjust height as needed
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    enabled: _isEditing,
+                    decoration: InputDecoration(
+                      labelText: 'First Name',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    enabled: _isEditing,
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    enabled: _isEditing,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _isEditing
+                  ? () {
+                      // Add function to handle user update here
+                    }
+                  : null,
+              child: Text(
+                'Save Changes',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return Colors.grey; // Disabled button color
+                    }
+                    return Colors.blue; // Enabled button color
+                  },
+                ),
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                ),
+                shape: MaterialStateProperty.all<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
