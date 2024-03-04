9188e973a767b470e5b90f43c465f8746756e8b3
PrashantBista <prashantbist64@gmail.com>
2024-03-04 15:24:22 +0545

    dashboard view

--- lib/views/dashboard/dashboard_view.dart
+++ lib/views/dashboard/dashboard_view.dart
@@ -1,59 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:pandabar/main.view.dart';
-import 'package:pandabar/model.dart';
 import 'package:vintuff_thrift/views/dashboard/dashboard_home.dart';
-import 'package:vintuff_thrift/views/dashboard/dashboard_notification.dart';
+import 'package:vintuff_thrift/views/dashboard/dashboard_search.dart';
 import 'package:vintuff_thrift/views/dashboard/dashboard_profile.dart';
-import 'package:vintuff_thrift/views/dashboard/dashnoard_message.dart';
+import 'package:vintuff_thrift/views/dashboard/cart_view.dart';
 
 class DashboardView extends StatefulWidget {
-  const DashboardView({super.key});
+  const DashboardView({Key? key}) : super(key: key);
 
   @override
   State<DashboardView> createState() => _DashboardViewState();
 }
 
 class _DashboardViewState extends State<DashboardView> {
-  String page = 'Grey';
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const CartView(),
+    const DashboardSearch(),
+    const MyProfile(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      bottomNavigationBar: PandaBar(
-        buttonData: [
-          PandaBarButtonData(id: 'Home', icon: Icons.home, title: 'Home'),
-          PandaBarButtonData(
-              id: 'Cart', icon: Icons.shopping_basket, title: 'Cart'),
-          PandaBarButtonData(
-              id: 'Notification',
-              icon: Icons.favorite_outline,
-              title: 'favorite'),
-          PandaBarButtonData(
-              id: 'Profile', icon: Icons.people, title: 'messages'),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.blue, // Change the background color
+        fixedColor: const Color.fromARGB(
+            255, 224, 147, 147), // Change the selected item color
+        unselectedItemColor: Colors.grey, // Change the unselected item color
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_basket),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Profile',
+          ),
         ],
-        onChange: (id) {
-          setState(() {
-            page = id;
-          });
-        },
-        onFabButtonPressed: () {},
       ),
-      body: Builder(
-        builder: (context) {
-          switch (page) {
-            case 'Home':
-              return const HomePage();
-            case 'Add to Cart':
-              return DashboardMessage();
-            case 'Notification':
-              return const DashboardNoitification();
-            case 'Profile':
-              return const MyProfile();
-            default:
-              return const HomePage();
-          }
-        },
-      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
